201dc97f0919ed4fdad83aed185c18017bbeeb9d
Kuddusi <kuddusic@gmail.com>
2018-09-06 16:25:48 +0300

    Removed root page

--- weatherService.py
+++ weatherService.py
@@ -2,10 +2,6 @@ from flask import Flask,request,jsonify
 from weather import Weather
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
 @app.route("/weather/getWeatherInfo/<city>")
 def getWeather(city):
     #city = request.args.get("city",default="Istanbul",type = str)    
