cd86383fb7c1d10b382d2116df8e0c48ffadd1ce
Kuddusi <kuddusic@gmail.com>
2018-09-07 11:10:27 +0300

    renamed app to application to deploy on AWS-EB

--- weatherService.py
+++ weatherService.py
@@ -1,10 +1,12 @@
 from flask import Flask,request,jsonify
 from weather import Weather
-app = Flask(__name__)
+application  = Flask(__name__)
 
-@app.route("/weather/getWeatherInfo/<city>")
+@application.route("/weather/getWeatherInfo/<city>")
 def getWeather(city):
     #city = request.args.get("city",default="Istanbul",type = str)    
     data = Weather().getInfo(city=city)
 
     return data
+if __name__=="__main__":
+    application .run()
