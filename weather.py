fafc2d512c1402c042d2e387f6d770c2c9a7db18
Kuddusi <kuddusic@gmail.com>
2018-09-14 10:43:32 +0300

    cosmetic changes

--- weather.py
+++ weather.py
@@ -1,55 +1,52 @@
 import json
 import urllib2
 
+
 class Weather():
     def __init__(self):
-        self.weatherBaseUrl = "http://api.openweathermap.org/data/2.5/"        
+        self.weatherBaseUrl = "http://api.openweathermap.org/data/2.5/"
         self.appkey = "3d049d48e11ad8bf9a8a7d01c66135aa"
-    def getInfo(self,city="Istanbul", apptype="weather"):
-        if apptype=="weather":
-            application="weather"
+
+    def getInfo(self, city="Istanbul", apptype="weather"):
+        # type: (object, object) -> object
+        if apptype == "weather":
+            application = "weather"
             if city.isalpha():
                 querykey = "q=%s" % city
-            else: # it meant id is supplied
+            else:  # it meant id is supplied
                 querykey = "id=%s" % city
-            finalUrl = "%s%s?APPID=%s&%s" % (self.weatherBaseUrl,application,self.appkey,querykey)
-            jsondata,errorMessage = self.getDataAsJSON(finalUrl)
+            finalUrl = "%s%s?APPID=%s&%s" % (self.weatherBaseUrl, application, self.appkey, querykey)
+            jsondata, errorMessage = self.getDataAsJSON(finalUrl)
             retValue = {}
             if errorMessage is not None:
                 retValue["error"] = errorMessage
                 return json.dumps(retValue)
             else:
-                temp = int(jsondata["main"]["temp"] - 273.15)                
-                retValue["error"]= "0"
+                temp = int(jsondata["main"]["temp"] - 273.15)
+                retValue["error"] = "0"
                 retValue["temp"] = temp
-            return json.dumps(retValue)             
-        
-    def getDataAsJSON(self,url):
-        errorMessage = None
+            return json.dumps(retValue)
+
+    def getDataAsJSON(self, url):
+        error_message = None
         jsondata = None
         try:
             response = urllib2.urlopen(url)
             data = response.read()
         except urllib2.HTTPError as e:
-            errorMessage = "Exception:%s,%s" % (e.code,e.read())
-            return (jsondata,errorMessage)
+            error_message = "Exception:%s,%s" % (e.code, e.read())
+            return (jsondata, error_message)
         except Exception as e:
-            errorMessage = "URLLib Exception:%s" % str(e)
-            return (jsondata,errorMessage)
+            error_message = "URLLib Exception:%s" % str(e)
+            return (jsondata, error_message)
         try:
-            jsondata = json.loads(data)            
+            jsondata = json.loads(data)
         except ValueError as e:
-            errorMessage = "JSON Decode Error: %s on pos:%d, col:%d" % (e.msg, e.pos,e.colno)
-        if jsondata["cod"]!=200:
-            errorMessage = jsondata["message"]
-        return (jsondata,errorMessage)
-if __name__=="__main__":
+            error_message = "JSON Decode Error: %s on pos:%d, col:%d" % (e.msg, e.pos, e.colno)
+        if jsondata["cod"] != 200:
+            error_message = jsondata["message"]
+        return (jsondata, error_message)
+
+
+if __name__ == "__main__":
     print(Weather().getInfo())
-
-    
-
-
-
-
-
-
--- weatherService.py
+++ weatherService.py
@@ -1,12 +1,16 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 from weather import Weather
-application  = Flask(__name__)
+
+application = Flask(__name__)
+
 
 @application.route("/weather/getWeatherInfo/<city>")
 def getWeather(city):
-    #city = request.args.get("city",default="Istanbul",type = str)    
+    # city = request.args.get("city",default="Istanbul",type = str)
     data = Weather().getInfo(city=city)
 
     return data
-if __name__=="__main__":
-    application .run()
+
+
+if __name__ == "__main__":
+    application.run()
